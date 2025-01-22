cf44093c39e13cee3885db4f9e051d610ff5ca95
Tshindimpho <mudzu13@gmail.com>
2025-01-23 00:23:15 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from './models/contact.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ContactBookUI';
+  
+  http = inject(HttpClient)
+   
+  contacts$ = this.getContacts();
+  
+  private getContacts(): Observable<Contact[]>{
+    return this.http.get<Contact[]>('https://localhost:7145/api/Contacts');
+  }
 }
+
+
